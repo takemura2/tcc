74137cf4224d2b81019022caefa1a99bd570f3f4
takemura2 <takemura2@gmail.com>
2018-11-24 04:35:01 +0000

    change by 1

--- tcc.c
+++ tcc.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    fprintf(stderr, "引数の個数が正しくありません11\n");
+    fprintf(stderr, "引数の個数が正しくありません14\n");
     //change by 1 and 2 marged
     return 1;
   }
